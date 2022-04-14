8f156873b88f4847fd8b767bcd9fe1dea80d8900
Idravis69 <bettoumi.idris@gmail.com>
2022-04-14 11:50:21 +0200

    bobo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,6 +25,7 @@
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
+            
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
@@ -33,11 +34,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+                
 
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
