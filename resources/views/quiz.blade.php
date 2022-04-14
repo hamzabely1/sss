96751bf0613f16bedaee4d423a4fd53d88ecc4fb
hamzabely1 <96245936+hamzabely1@users.noreply.github.com>
2022-04-14 12:35:05 +0200

    g

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -1,5 +1,4 @@
 
-<form action="/action_page.php">
     <p>Vous etes facilement derage par les lumieres,les bruit, les odeurs.certaines sensation tactiles?</p>
     <input type="radio" id="html" name="fav_language" value="HTML">
     <label for="html">Non</label><br>
@@ -13,107 +12,107 @@
     <br>
 
     <p>vous remarque le changement subtils dans votre environnement</p>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+      <input type="radio" id="html" name="f" value="i">
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="f" value="i">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="f" value="i">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="f" value="i">
       <label for="javascript">oui</label>
 
     <br>
 
     <p>vous etes sensible aux ambiances</p>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+      <input type="radio" id="html" name="o" value="o">
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="o" value="o">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="o" value="o">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="o" value="o">
       <label for="javascript">oui</label>
 
     <br>
 
     <p>vous reaction emotionneles sont instenses,qu'elles soinet posivite et negavite?</p>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+      <input type="radio" id="html" name="r" value="r">
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="r" value="r">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="r" value="r">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="r" value="r">
       <label for="javascript">oui</label>
 
     <br>
 
     <p>vous etes facilment touche ou emu</p>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+      <input type="radio" id="html" name="b" value="b">
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="b" value="b">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="b" value="b">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="b" value="b">
       <label for="javascript">oui</label>
 
     <br>
 
     <p>vos proches vous decrivent comme qual'qu'un de sensible</p>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+      <input type="radio" id="html" name="ae" value="a">
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="a" value="a">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="a" value="a">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="a" value="a">
       <label for="javascript">oui</label>
 
     <br>
 
     <p>vous nouez des relations profondes avec les autres.qui vous nourissent recproquement</p>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+      <input type="radio" id="html" name="j" value="j">
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="j" value="CSS">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="je" value="j">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="j" value="j">
       <label for="javascript">oui</label>
 
     <br>
 
     <p>vous etes attentif a vos emotions et a celles des autres</p>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+      <input type="radio" id="html" name="d" value="d">
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="d" value="d">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="d" value="d">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="d" value="d">
       <label for="javascript">oui</label>
 
     <br>
 
     <p>vos approuve reguilerement le besoin de vous isoler pourt vous</p>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+      <input type="radio" id="html" name="m" value="m">
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="m" value="m">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="m" value="Jmt">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="m" value="m">
       <label for="javascript">oui</label>
 
     <br>
 
     <p>vous apprecisez les ouvers d'art </p>
       <label for="html">Non</label><br>
-      <input type="radio" id="css" name="fav_language" value="CSS">
+      <input type="radio" id="css" name="h" value="h">
       <label for="css">plutot non</label><br>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="h" value="h">
       <label for="javascript">plutot oui</label>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+      <input type="radio" id="javascript" name="h" value="h">
       <label for="javascript">oui</label>
-  </form>
+
