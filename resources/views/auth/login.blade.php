4dd034b347ff76ef94acdb8dc982fe0e5e8ecefd
Idravis69 <bettoumi.idris@gmail.com>
2022-04-14 11:53:08 +0200

    essaie

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,12 +26,7 @@
             </div>
 
             
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+            
 
             <div class="flex items-center justify-end mt-4">
                 
